d1d9dd185348394af7f7a5db1b1c5cc5f686c015
IfyN <ifynwosu0@gmail.com>
2024-01-10 20:12:10 +0100

    get audio buttons and audio

---
+++ script.js
@@ -0,0 +1,22 @@
+//Get buttons
+let clap = document.getElementById("clap");
+let hihat = document.getElementById("hihat");
+let kick = document.getElementById("kick");
+let openhat = document.getElementById("openhat");
+let boom = document.getElementById("boom");
+let ride = document.getElementById("ride");
+let snare = document.getElementById("snare");
+let tom = document.getElementById("tom");
+let tink = document.getElementById("tink");
+
+//Get audio element
+let clapSound = document.getElementById("clapSound");
+let hatSound = document.getElementById("hatSound");
+let kickSound = document.getElementById("kickSound");
+let openSound = document.getElementById("openSound");
+let boomSound = document.getElementById("boomSound");
+let rideSound = document.getElementById("rideSound");
+let snareSound = document.getElementById("snareSound");
+let tomSound = document.getElementById("tomSound");
+let tinkSound = document.getElementById("tinkSound");
+
